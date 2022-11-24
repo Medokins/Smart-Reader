68ee3966664d70935e701ef875a583d3e9105302
Medokins <nikodemszwast@gmail.com>
2022-11-24 18:23:09 +0100

    remove truncate_label method for now

--- textReader/image_processor.py
+++ textReader/image_processor.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2
-from typing import Tuple 
+from typing import Tuple
 
 BATCH = ['imgs']
 
@@ -16,23 +16,6 @@ class ImageProcessor:
         self.image_size = image_size
         self.line_mode = line_mode
 
-    def truncate_label(text: str, max_text_len: int) -> str:
-        """
-        Function ctc_loss can't compute loss if it cannot find a mapping between text label and input
-        labels. Repeat letters cost double because of the blank symbol needing to be inserted.
-        If a too-long label is provided, ctc_loss returns an infinite gradient.
-        From: https://github.com/githubharald/SimpleHTR/blob/master/src/preprocessor.py
-        """
-        cost = 0
-        for i in range(len(text)):
-            if i != 0 and text[i] == text[i - 1]:
-                cost += 2
-            else:
-                cost += 1
-            if cost > max_text_len:
-                return text[:i]
-        return text
-
     def process_img(self) -> None:
         """
         Scales image to self.image_size, convert image values to be in range [-1, 1] and transposes it
@@ -40,7 +23,7 @@ class ImageProcessor:
         """
         # there are damaged files in IAM dataset - use black image instead
         if self.image is None:
-            self.image = np.zeros(self.img_size[::-1])
+            self.image = np.zeros(self.image_size[::-1])
 
         # image scaling
         self.image = self.image.astype(np.float)
@@ -63,10 +46,10 @@ class ImageProcessor:
         self.image /= np.max(np.abs(self.image), axis=0)
 
 
-def main():
+def plot(path):
     import matplotlib.pyplot as plt
 
-    img = cv2.imread('data/img/a01/a01-000u/a01-000u-00-05.png', cv2.IMREAD_GRAYSCALE)
+    img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
     img_scaled = ImageProcessor(img, (256, 64))
     img_scaled.process_img()
 
@@ -74,6 +57,3 @@ def main():
     axarr[0].imshow(img, cmap='gray')
     axarr[1].imshow(np.transpose(img_scaled.image), cmap='gray')
     plt.show()
-
-if __name__ == '__main__':
-    main()
