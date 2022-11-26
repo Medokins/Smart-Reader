0d38c2950640164ea7cafe4ccce2ceaf6e74cc63
Medokins <nikodemszwast@gmail.com>
2022-11-26 15:27:14 +0100

    rewrite framework for ImageProcessor

--- textReader/image_processor.py
+++ textReader/image_processor.py
@@ -21,13 +21,8 @@ class ImageProcessor:
         blurred = cv2.GaussianBlur(self.image, (5, 5), 0)
         _, self.image = cv2.threshold(blurred, 0, 255, cv2.THRESH_OTSU)
         edged = cv2.Canny(blurred, 30, 150)
-        cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cnts = imutils.grab_contours(cnts)
-        print(cnts[0])
-        #cnts = sort_contours(cnts, method="left-to-right")[0]
-
-        cv2.imshow('test-img', self.image)
-        cv2.waitKey(0)
+        contours = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours = imutils.grab_contours(contours)
 
 def main():
     path = 'data/img/a02/a02-000/a02-000-00-05.png'
