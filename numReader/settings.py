b0746a173ce03ce43ab427d7cdb03240e555840a
Medokins <nikodemszwast@gmail.com>
2022-12-02 04:21:37 +0100

    add settings file

---
+++ numReader/settings.py
@@ -0,0 +1,5 @@
+# closing parameters
+KERNEL_SIZE = 3
+CLOSING_INTERATIONS = 1
+# Area in which to filter noise on an image (white pixel will be turned black if there are less than x white pixels around it)
+MIN_AREA_FILTER = 20
