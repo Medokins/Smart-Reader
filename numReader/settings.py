9176d99e8725369b382710d26a7cf7b230630abe
Medokins <nikodemszwast@gmail.com>
2022-12-03 15:44:25 +0100

    add batch_size and epochs variables

--- numReader/settings.py
+++ numReader/settings.py
@@ -3,3 +3,6 @@ KERNEL_SIZE = 3
 CLOSING_INTERATIONS = 1
 # Area in which to filter noise on an image (white pixel will be turned black if there are less than x white pixels around it)
 MIN_AREA_FILTER = 20
+
+BATCH_SIZE = 128
+EPOCHS = 50
