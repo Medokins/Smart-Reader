6f76edbbc3729963268fca4ecf434bb273da80b6
Medokins <nikodemszwast@gmail.com>
2022-12-04 12:45:51 +0100

    remove readDigit function

--- numReader/model.py
+++ numReader/model.py
@@ -1,7 +1,5 @@
 import numpy as np
 import tensorflow as tf
-import cv2
-from data_preprocessor import preprocessImage
 from sklearn.model_selection import train_test_split
 from settings import BATCH_SIZE, EPOCHS
 
@@ -24,7 +22,6 @@ def trainNumReader():
 
     (trainX, testX, trainY, testY) = train_test_split(digits_data, digits_labels, test_size=.2, random_state=42)
 
-
     model = tf.keras.models.Sequential()
     # takes our 28x28 and makes it 1x784
     model.add(tf.keras.layers.Flatten())
@@ -32,33 +29,27 @@ def trainNumReader():
     model.add(tf.keras.layers.Dense(128, activation = tf.nn.relu))
     # fully-connected layer, 128 units, relu activation
     model.add(tf.keras.layers.Dense(128, activation = tf.nn.relu))
+    # outpu layer with each node coresponding to single digit 0-9
     model.add(tf.keras.layers.Dense(10, activation = tf.nn.softmax))
     
     aug = tf.keras.preprocessing.image.ImageDataGenerator(
-        rotation_range=10,
-        zoom_range=0.05,
-        width_shift_range=0.1,
-        height_shift_range=0.1,
-        shear_range=0.15,
-        horizontal_flip=False,
-        fill_mode="nearest")
+        rotation_range = 10,
+        zoom_range = .05,
+        width_shift_range = .1,
+        height_shift_range = .1,
+        shear_range = .15,
+        horizontal_flip = False,
+        fill_mode = "nearest")
 
     # training
-    model.compile(optimizer='adam' , loss='sparse_categorical_crossentropy', metrics=["accuracy"])
+    model.compile(optimizer='adam' , loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     model.fit(
         aug.flow(trainX, trainY, batch_size = BATCH_SIZE),
         validation_data = (testX, testY),
         steps_per_epoch = len(trainX) // BATCH_SIZE,
         epochs=EPOCHS)
 
-    # # evaluation
+    # evaluation
     val_loss, val_acc = model.evaluate(testX, testY)
     print(val_loss, val_acc)
     model.save('newModel.model')
-
-def readDigit(name):
-    im = cv2.imread(f'handwrittenNumbers/{name}.png', cv2.IMREAD_GRAYSCALE)
-    convertedImage = preprocessImage(im)
-    new_model = tf.keras.models.load_model('newModel.model')
-    predictions = new_model.predict([convertedImage])
-    print(np.argmax(predictions[0]))
