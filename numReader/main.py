8a6e301312e066aa154dbe3e2151f091736a7702
Medokins <nikodemszwast@gmail.com>
2022-12-05 16:08:27 +0100

    add camera live-view

--- numReader/main.py
+++ numReader/main.py
@@ -1,6 +1,15 @@
 from data_preprocessor import getBoundingBoxes, readDigits
+import cv2
 
 if __name__ == '__main__':
     name = 'test'
-    coordinates_array = getBoundingBoxes(name, visualize=False)
+    vid = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    while True:
+        ret, frame = vid.read()
+        coordinates_array = getBoundingBoxes('CamerView', visualize=True, img=frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    vid.release()
+    cv2.destroyAllWindows()
     readDigits(coordinates_array, name)
