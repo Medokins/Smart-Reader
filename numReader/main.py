4cdefb5428c0d4e335ab3b227709d1edc4749687
Nikodem <nikodemszwast@gmail.com>
2022-12-13 22:50:45 +0100

    add framework for gui

--- numReader/main.py
+++ numReader/main.py
@@ -5,18 +5,19 @@ import PySimpleGUI as sg
 def create_gui():
     sg.theme('DarkGreen3')   
 
-    layout = [  [sg.Column([[sg.Button('Exit' )]], element_justification='right', expand_x=True)],
+    layout = [
+            [sg.Column([[sg.Button('Exit' )]], element_justification='right', expand_x=True)],
             [sg.Text("Choose a file: ", pad=(0,10)), sg.Input(), sg.FileBrowse(key="-IN-")],
-            [sg.Button("Submit", pad=(0,10))],
+            [sg.Button("Submit", pad=(0,10))], [sg.Button("Live View", pad=(0,10))],
             [sg.Text("", key='-OUTPUT-', pad=(0,10))],
-            [sg.Text("Image:", key='-TXT-', size=(25, 2), pad=(0, 0), expand_x=True, expand_y=False, visible=False),
-            sg.Text("Numbers:", key='-TXT2-', size=(25, 2), pad=(0, 0), expand_x=True, expand_y=False, visible=False)],
-            [sg.Image(key='-IMAGE-',  size=(256, 256), pad=(0,0), expand_x=False, expand_y=False),
-            sg.Image(key='-IMAGE2-', size=(256, 256), pad=(0,0), expand_x=True, expand_y=False)]
+            [sg.Text("Image:", key='-TXT-', size=(25, 2), pad=(0, 0), expand_x=True, expand_y=False, visible=False), sg.Text("Numbers:", key='-TXT2-', size=(25, 2), pad=(0, 0), expand_x=True, expand_y=False, visible=False)],
+            [sg.Image(key='-IMAGE-',  size=(256, 256), pad=(0,0), expand_x=False, expand_y=False), sg.Image(key='-IMAGE2-', size=(256, 256), pad=(0,0), expand_x=True, expand_y=False)]
             ]
 
     window = sg.Window('Reading numbers', layout, size=(800, 600), finalize=True)
-    
+    last_frame = None
+    event = None
+
     while True:
         event, values = window.read()
 
@@ -24,32 +25,30 @@ def create_gui():
             break
 
         if event == "Submit":
-            # predict from given picture
-            # val = model(values["-IN-"])
-
-            # path, label = model_labels(val)
-           # window['-OUTPUT-'].update(value = f"Model prediction: {label}")
-
-            imgbytes = get_image(values['-IN-'])
-            window['-TXT-'].update(visible=True)
-            window['-TXT2-'].update(visible=True)
-            #['-IMAGE-'].update(data = imgbytes)
-            #imgbytes = get_image(path)
-            #window['-IMAGE2-'].update(data = imgbytes)
-
-
-window.close()
-
-
-if __name__ == '__main__':
-    name = 'test'
-    vid = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-    while True:
-        ret, frame = vid.read()
-        coordinates_array = getBoundingBoxes('CamerView', visualize=True, img=frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+            name = 'test'
+            coordinates_array = getBoundingBoxes(name, visualize=True)
+            readDigits(coordinates_array, name)
             break
 
-    vid.release()
-    cv2.destroyAllWindows()
-    readDigits(coordinates_array, name)
+        if event == "Live View":
+            break
+
+    window.close()
+
+    if event == "Live View":
+        vid = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+        while True:
+            _, frame = vid.read()
+            last_frame = frame.copy()
+            getBoundingBoxes('CamerView', visualize=True, img=frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+                
+        vid.release()
+        cv2.destroyAllWindows()
+        coordinates_array = getBoundingBoxes('Last Frame', visualize=False, img=last_frame)
+        last_frame_gray = cv2.cvtColor(last_frame, cv2.COLOR_BGR2GRAY)
+        readDigits(coordinates_array, name=None, img=last_frame_gray)
+
+if __name__ == '__main__':
+    create_gui()
