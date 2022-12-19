00edc46025344628b1d9f4e35c119ed05add5bff
Nikodem <nikodemszwast@gmail.com>
2022-12-19 14:37:56 +0100

    remove unnecessary settings variables

--- numReader/settings.py
+++ numReader/settings.py
@@ -3,6 +3,4 @@ KERNEL_SIZE = 3
 CLOSING_INTERATIONS = 1
 # Area in which to filter noise on an image (white pixel will be turned black if there are less than x white pixels around it)
 MIN_AREA_FILTER = 20
-
-BATCH_SIZE = 256
-EPOCHS = 50
+EPOCHS = 10
